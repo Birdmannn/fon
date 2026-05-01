770655c47ed2f03f6b4e929c22b4d327f1db8312
Birdmannn <tortipascal5000@gmail.com>
2026-05-01 17:57:48 +0100

    removed `/create` page

--- frontend/app/create/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import CreateCampaignModalContent from "./_components/CreateCampaignModalContent";
-
-export default function CreateCampaignPage() {
-  return <CreateCampaignModalContent mode="page" />;
-}
