e2d08b87728149d6df6bd20af0ec87d3baf1060b
Birdmannn <tortipascal5000@gmail.com>
2026-05-01 18:09:40 +0100

    customized 404

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="min-h-screen flex items-center justify-center px-6 text-center">
+      <h1 className="text-3xl sm:text-4xl font-bold">Oops, 404 Not Found</h1>
+    </main>
+  );
+}
